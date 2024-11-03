4e880df6866efde34b69e5951a39139d15d8f524
0PrashantYadav0 <prashantyadav09783@gmail.com>
2024-11-03 18:33:33 +0530

    added chatbot ui and integration

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,8 +1,16 @@
-import { buttonVariants } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 import { WalletSelector } from "./WalletSelector";
+import { useState } from "react";
+import ChatBot from "./ChatBot";
 
 export function Header() {
+  const [chatBot, setChatBot] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setChatBot(!chatBot);
+  };
+
   return (
     <div className="flex items-center backdrop-blur-lg justify-between px-4 py-2 max-w-screen-xl mx-auto w-full flex-wrap md:flex-nowrap">
       <h1 className="display font-mono">
@@ -16,12 +24,13 @@ export function Header() {
           <Link className={buttonVariants({ variant: "link" })} to={"/create-scholarship"}>
             Create
           </Link>
+          <Button variant={"link"} onClick={handleClick}>ChatBot</Button>
           <Link className={buttonVariants({ variant: "link" })} to={"/apply"}>
             Apply here
           </Link>
         </>
-
         <WalletSelector />
+        {chatBot && <ChatBot />}
       </div>
     </div>
   );
